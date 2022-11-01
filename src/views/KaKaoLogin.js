79860db0bbd6a9ae81f9f8118cbf995e335be484
jinhee kim <wlsgml0229@naver.com>
2022-11-02 02:16:31 +0900

    카카오로그인 인가코드 백단으로 전송

--- src/views/KaKaoLogin.js
+++ src/views/KaKaoLogin.js
@@ -1,37 +1,30 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-import { REDIRECT_URI, REST_API_KEY } from "../secretConfig";
 import { useLocation } from "react-router-dom";
+import { token } from "../utils/token";
+import { useNavigate } from "react-router-dom";
 
 const KakaoLogin = () => {
   const location = useLocation();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const KAKAO_CODE = location.search.split("=")[1];
   console.log("kakao_code", KAKAO_CODE);
-  let grant_type = "authorization_code";
+
   useEffect(() => {
     // 백엔드로 인가코드보내기
     axios
-      .post(
-        `https://kauth.kakao.com/oauth/token?
-        grant_type=${grant_type}
-        &client_id=${REST_API_KEY}
-        &redirect_uri=${REDIRECT_URI}
-        &code=${KAKAO_CODE}`,
-        {
-          headers: {
-            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-          },
-        }
-      )
+      .post("/login", { code: KAKAO_CODE })
       .then((res) => {
-        console.log(res);
-        // res에 포함된 토큰 받아서 원하는 로직을 하면된다.
-        if (res.access_token) {
-          localStorage.setItem("token", res.access_token);
+        console.log("tokenSuccess", res);
+        if (res.data.response) {
+          token.set("user_token", res.data.response.data);
+          navigate("/home");
         }
+      })
+      .catch((err) => {
+        alert(err);
       });
-  }, []);
+  });
 
   return <div>KaKaoLogin</div>;
 };
