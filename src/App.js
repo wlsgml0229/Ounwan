56bb1e498fb71bcb4c29b6c318f69fab4887988c
jinhee kim <wlsgml0229@naver.com>
2022-11-17 00:44:44 +0900

    로그인 관련 라우터 수정 완료

--- src/App.js
+++ src/App.js
@@ -3,31 +3,27 @@ import Login from "./views/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import KakaoLogin from "./views/KaKaoLogin";
 import Home from "./views/Home";
-// import PrivateRoute from "./views/PrivateRoute";
+import PrivateRoute from "./views/PrivateRoute";
 import NotFound from "./views/NotFound";
 import Navbar from "./views/Navbar";
 import RoomDetail from "./views/room/RoomDetail";
-
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
-          {/* 
+
           <Route
             index
-            path="/"
             element={
               <PrivateRoute>
                 <Route element={<Navbar />}></Route>
-                <Home />
+                <Route element={<Home />} path="/" />
+                <Route element={<RoomDetail />} path="/roomDetail/:id" />
               </PrivateRoute>
             }
-          /> */}
-
-          <Route element={<Home />} path="/" />
-          <Route element={<RoomDetail />} path="/roomDetail/:id" />
+          />
           <Route path="/kakaoLogin" element={<KakaoLogin />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
--- src/views/room/RoomDetail.js
+++ src/views/room/RoomDetail.js
@@ -1,8 +1,15 @@
+// import axios from "axios";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const RoomDetail = () => {
   const { id } = useParams();
   console.log(id);
+  const requestRoomDetail = () => {};
+
+  useEffect(() => {
+    requestRoomDetail();
+  });
   return <div>Room 상세페이지</div>;
 };
 
