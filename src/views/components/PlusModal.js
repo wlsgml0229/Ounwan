becd95f675823abe0f237a3c722e84a646a28869
jinhee kim <wlsgml0229@naver.com>
2022-11-23 07:04:03 +0900

    방 생성하기 작업중

--- src/views/components/PlusModal.js
+++ src/views/components/PlusModal.js
@@ -1,30 +1,38 @@
 import "../../scss/common/plusmodal.scss";
-import {useState } from "react";
+import { useState } from "react";
 
 const PlusModal = () => {
   const [plusBtn, setPlusBtn] = useState(false);
+  const [roomModal, setRoomModal] = useState(false);
+  const handleOpen = () => setRoomModal(true);
+  const handleClose = () => setRoomModal(false);
 
   const clickPlusBtn = () => {
-    setPlusBtn(!plusBtn)
-  }
+    setPlusBtn(!plusBtn);
+  };
+
   return (
-  <div className="plus-modal" onClick={clickPlusBtn}>
-    
-    <div className="plus-btn">
-      +
-    </div>
-    {plusBtn && 
-      <div className="plus-modal-wrap">
-        <div className="plus-modal-content">
-          <ul>
-            <li>
-              방 만들기
-            </li>
-          </ul>
-        </div>
+    <div className="plus-modal">
+      <div className="plus-btn" onClick={!roomModal && clickPlusBtn}>
+        +
       </div>
-    }
-  </div>
+      {plusBtn && (
+        <div className="plus-modal-wrap" onClick={clickPlusBtn}>
+          <div className="plus-modal-content">
+            <ul>
+              <li onClick={handleOpen}>💪 방 만들기</li>
+            </ul>
+          </div>
+        </div>
+      )}
+
+      {/* {roomModal && (
+        <div className="room-modal">
+          <b>🥳 방 만들기</b>
+          <p></p>
+        </div>
+      )} */}
+    </div>
   );
 };
 
---
+++ src/views/room/RoomCreate.js
@@ -0,0 +1,9 @@
+const RoomCreate = () => {
+  <div className="room-create">
+    <div className="header-wrap">
+      <p>돌아가기</p>
+      <h2>방 만들기 </h2>
+    </div>
+    <div className="room-create-content"></div>
+  </div>;
+};
